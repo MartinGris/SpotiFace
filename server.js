40effc21cda1114b8bf73fc3fca8c54123c2b7b7
Martin <martin@grisard.net>
2014-11-11 21:03:38 +0100

    facebook login added

--- server.js
+++ server.js
@@ -23,6 +23,36 @@ db.connect(function(err){
     if (err) console.log(err)
 });
 
+
+var passport = require('passport')
+  , FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+    clientID: 656991001080494,
+    clientSecret: 57762c91c1d1bc4ed348334a19b7a015,
+    callbackURL: "http://www.example.com/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate(..., function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
+
+
 app.get('/', function(req, res){
     console.log('hello world');
 
