bb8de68d8c9587c752ed596d17ec2e979bcfb043
Martin <martin@grisard.net>
2014-11-11 20:32:07 +0100

    change port

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ io.on('connection', function(socket){
 });
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port      = process.env.OPENSHIFT_NODEJS_PORT;
 
 if (typeof ipaddress === "undefined") {
     //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
