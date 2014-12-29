4c52b5e8592313a077942f3f941ad54baac48454
Martin <martin@grisard.net>
2014-12-29 23:27:59 +0100

    bugfix

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.get('/logout', function(req, res, next) {
 });
 // app.get('/spoti', ensureAuthenticated, function(req, res){
 app.get('/spoti', ensureAuthenticated, function(req, res, next){
-    res.render('spoti' { userId: res);
+    res.render('spoti', { userId: res});
 
 });
 
