c08af8a637ad233522b37413f8977909e662ffee
Martin <martin@grisard.net>
2014-12-29 20:15:41 +0100

    a

--- public/javascripts/spotify.js
+++ public/javascripts/spotify.js
@@ -1,3 +1,8 @@
+$(document).ready(function () {
+   $('#searchInput').keyup(function () { alert('test'); });
+});
+
+
 $("#searchInput").on("change paste keyup", function() {
    alert($(this).val()); 
 });
