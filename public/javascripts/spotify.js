9b9ad2891fbc5a36069895b56fcb3b60c010953c
Martin <martin@grisard.net>
2014-12-30 20:23:46 +0100

    put function clientside

--- public/javascripts/spotify.js
+++ public/javascripts/spotify.js
@@ -53,24 +53,41 @@ function searchAjaxRequest( searchInput ){
 }
 
 function addSong( id ){
-    $.ajax( "https://api.spotify.com/v1/tracks/" + id )
-	.done(function(data) {
-		console.log(data);
-		
-    	var rowString = "";
-    	
-    	rowString = "<tr id ='" + id + "'> <td>" + data.artists[0].name + "</td>";
-    	rowString += "<td>" + data.name + "</td>";
-    	rowString += "<td class='text-center'><a><span onClick='javascript: playStopSong( \" " + data.preview_url + " \", $(this) ); toggleButton( $(this) );' class='playbutton glyphicon glyphicon-play-circle'></span></a>";
-    	rowString += " <a><span onClick='javascript: deleteSong(\""+ data.id + "\")' class='glyphicon glyphicon-trash'></span></a> </td> </tr>";
-    	
-    	$("#songList tbody").append( rowString );
-		
-	  })
-	    
-    .fail(function() {
-      // alert( "error" );
-    })
+	$.ajax({
+		  type: "PUT",
+		  url: "http://spotiface-grisard.rhcloud.com/spoti/user/" + userId +  "/songs",
+		  dataType: "json",
+		  data: {songId: id},
+		  statusCode: {
+			  423: function( message ) {
+			      alert( message );
+			    }
+			  }
+		}).done(function( data ){
+			
+			$.ajax( "https://api.spotify.com/v1/tracks/" + id )
+			.done(function(data) {
+				console.log(data);
+				
+				var rowString = "";
+				
+				rowString = "<tr id ='" + id + "'> <td>" + data.artists[0].name + "</td>";
+				rowString += "<td>" + data.name + "</td>";
+				rowString += "<td class='text-center'><a><span onClick='javascript: playStopSong( \" " + data.preview_url + " \", $(this) ); toggleButton( $(this) );' class='playbutton glyphicon glyphicon-play-circle'></span></a>";
+				rowString += " <a><span onClick='javascript: deleteSong(\""+ data.id + "\")' class='glyphicon glyphicon-trash'></span></a> </td> </tr>";
+				
+				$("#songList tbody").append( rowString );
+				
+			})
+			
+			.fail(function() {
+				// alert( "error" );
+			})
+			
+			
+		});
+	
+	
 }
 
 function deleteSong( id ){
