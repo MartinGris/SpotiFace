58dd9211e1b636bbda00d0f39f34a7945ee3ca67
mg <martin@grisard.net>
2015-01-21 20:28:08 +0100

    python

--- python/spotiface.py
+++ python/spotiface.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python3
 import spotify
 import time
 import pymysql
 
 playlistId = '72pl3n7OuAXXN5O0n5TU2A'
 
-session = spotify.Session()
+config = spotify.Config()
+
+config.load_application_key_file('/opt/spotiface/spotify_appkey.key')
+
+session = spotify.Session(config)
 
 session.login('1183081422', '3rdf3rk3l')
 
@@ -36,7 +41,7 @@ playlist.remove_tracks( range( len( tracks ) ) )
 
 session.process_events()
 
-time.sleep(1)
+time.sleep(2)
 
 conn = pymysql.connect(host='93.157.51.165', unix_socket='/tmp/mysql.sock', user='spotiUser', passwd="3rdf3rk3lSpoti", db='spotiface')
 cur = conn.cursor()
