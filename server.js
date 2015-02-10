a2a2ea656983cbdf8059fd37fb2eb666677c974b
Martin <martin@grisard.net>
2015-02-10 21:10:15 +0100

    logging

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ passport.use(new FacebookStrategy({
 	  }).setAccessToken(LONGLIFETOKEN);
     
     process.nextTick(function() {
-        console.log( "userlogin: " + profile.name.givenName );
+        console.log( "userlogin: " + profile.name );
                 
         var id = profile.id;
                 
